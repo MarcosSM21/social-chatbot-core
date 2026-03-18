ef84dfd6697cadc904d1e4cf3fa9ced8250e9baa
marcos <marcossanchez969@gmail.com>
2026-03-18 14:54:53 +0100

    feat: añadido un poco de personalización

--- app/channels/local_channel.py
+++ app/channels/local_channel.py
@@ -1,18 +1,20 @@
 from app.orchestrator.chat_orquestrator import ChatOrchestrator
+from app.core.settings import Settings
 
 class LocalChannel:
-    def __init__(self, orchestrator: ChatOrchestrator) -> None:
+    def __init__(self, orchestrator: ChatOrchestrator, settings: Settings) -> None:
         self.orchestrator = orchestrator
+        self.settings = settings 
 
     def run(self) -> None:
-        print ("Local chat started. Write 'q' to quit." )
+        print (self.settings.bot_welcome_message)
 
         while True:
             user_message = input("You: ").strip()
 
             if user_message.lower() == "q":
-                print("Good bye!")
+                print(f"{self.settings.bot_name}: {self.settings.bot_goodbye_message}")
                 break
 
             response = self.orchestrator.handle_message(user_message)
-            print(f"Bot: {response}")
+            print(response)
