5cf1eb4802ecef4074175ddd9705d693e677a23c
marcos <marcossanchez969@gmail.com>
2026-03-19 09:26:08 +0100

    feat: añadido chat local persistente con JSON

--- app/models/chat.py
+++ app/models/chat.py
@@ -1,12 +1,34 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 
 @dataclass
 class ChatMessage:
     role: str
     content: str
 
+    def to_dict(self) -> dict:
+        return asdict(self)
+    
+    @classmethod
+    def from_dict(cls, data: dict) ->"ChatMessage":
+        return cls(
+            role=data['role'], 
+            content=data['content']
+            )
+
 @dataclass
 class ChatTurn:
     user_message: ChatMessage
     assistant_message: ChatMessage
 
+    def to_dict(self) -> dict:
+        return {
+            "user_message": self.user_message.to_dict(),
+            "assistant_message": self.assistant_message.to_dict()
+        }
+    
+    @classmethod
+    def from_dict(cls, data: dict) -> "ChatTurn":
+        return cls(
+            user_message=ChatMessage.from_dict(data['user_message']),
+            assistant_message=ChatMessage.from_dict(data['assistant_message'])
+        )
