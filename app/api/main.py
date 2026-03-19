adad66297339154a13188675a8bd9372a90b5a42
marcos <marcossanchez969@gmail.com>
2026-03-19 12:52:06 +0100

    feat: añadir health e info api endopints

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 
-from app.api.schemas import MessageRequest, MessageResponse
+from app.api.schemas import (
+    MessageRequest,
+    MessageResponse,
+    HealthResponse,
+    InfoResponse)
+
+from app.core.settings import Settings
+
+
 
 app = FastAPI(
     title="social-chatbot-core API",
@@ -8,12 +16,29 @@ app = FastAPI(
     description="Interanl API para el chatbot social core project"
 )
 
+settings = Settings.from_env()
+
 @app.get("/")
 def root() -> dict:
     return {
         "message": "social-chatbot-core API is running",
-        "status": "ok"
+        "docs_url": "/docs",
+        "health_url": "/health",
+        "info_url": "/info",
     }
+
+@app.get("/health", response_model=HealthResponse)
+def health() -> HealthResponse:
+    return HealthResponse(status="ok")
+
+@app.get("/info", response_model=InfoResponse)
+def info() -> InfoResponse:
+    return InfoResponse(
+        service_name="social-chatbot-core",
+        version="0.1.0",
+        enviroment=settings.app_env,
+        llm_provider=settings.llm_provider
+    )
 
 
 @app.post("/messages", response_model=MessageResponse)
--- app/api/schemas.py
+++ app/api/schemas.py
@@ -8,3 +8,13 @@ class MessageResponse(BaseModel):
     user_message: str = Field(..., description="El mensaje del usuario")
     assistant_message: str = Field(..., description="La respuesta del asistente")
     session_id: str = Field(..., description="ID de la sesión")
+
+
+class HealthResponse(BaseModel):
+    status: str = Field(..., description="Estado de la API")
+    
+class InfoResponse(BaseModel):
+    service_name: str = Field(..., description="Nombre del servicio")
+    version: str = Field(..., description="Versión del servicio")
+    enviroment: str = Field(..., description="Entorno de ejecución")
+    llm_provider: str = Field(..., description="Proveedor de LLM utilizado")
