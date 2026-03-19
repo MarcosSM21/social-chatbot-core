aaa7a6957632bde7bee718bfbc4a5bb8535a7339
marcos <marcossanchez969@gmail.com>
2026-03-19 15:39:17 +0100

    docs: finalizar el resumen de fase 2

---
+++ app/core/container.py
@@ -0,0 +1,29 @@
+import uuid 
+
+from app.channels.local_channel import LocalChannel
+from app.core.settings import Settings
+from app.engine.response_engine import ResponseEngine
+from app.orchestrator.chat_orquestrator import ChatOrchestrator
+from app.services.conversation_service import ConversationService
+from app.storage.local_chat_repository import LocalChatRepository
+
+def build_chat_orchestrator(settings: Settings) -> ChatOrchestrator:
+    response_engine = ResponseEngine(settings=settings)
+    chat_repository = LocalChatRepository()
+    conversation_service = ConversationService(
+        response_engine,
+        chat_repository 
+    )
+    return ChatOrchestrator(conversation_service)
+
+
+def build_local_channel(settings: Settings) -> LocalChannel:
+    orchestrator = build_chat_orchestrator(settings)
+    session_id = str(uuid.uuid4())
+    return LocalChannel(
+        orchestrator, 
+        settings=settings, 
+        session_id=session_id
+        )
+
+
